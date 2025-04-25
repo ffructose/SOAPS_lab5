5d6772e0273cb085f5db6bfc0e1aa35862121a95
Anna Hnatiuk <losers2004@gmail.com>
2025-04-25 17:17:52 +0200

    2

--- auth-service/cmd/api/main.go
+++ auth-service/cmd/api/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-    mux := routes()
-    log.Println("Starting authentication service on port 8081...")
-    err := http.ListenAndServe(":8081", mux)
-    if err != nil {
-        log.Fatal(err)
-    }
+
+	mux := routes()
+	log.Println("Starting authentication service on port 8081...")
+	err := http.ListenAndServe(":8081", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- auth-service/go.mod
+++ auth-service/go.mod
@@ -2,4 +2,8 @@ module auth-service
 
 go 1.18
 
-require github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+)
